d40d8692df6c67a15955a31b33188c37b3ba76f3
Andrew <uandreic@gmail.com>
2025-05-16 03:38:57 +0000

    forgot to add SimVP

---
+++ src/model/simvp.py
@@ -0,0 +1,488 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from timm.models.convnext import ConvNeXtBlock
+
+from timm.layers import trunc_normal_, DropPath
+import math
+
+class ConvNeXtSubBlock(ConvNeXtBlock):
+    """A block of ConvNeXt."""
+
+    def __init__(self, dim, mlp_ratio=4., drop=0., drop_path=0.1):
+        super().__init__(dim, mlp_ratio=mlp_ratio,
+                         drop_path=drop_path, ls_init_value=1e-6, conv_mlp=True)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.LayerNorm, nn.GroupNorm, nn.BatchNorm2d)):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'gamma'}
+
+    def forward(self, x):
+        x = x + self.drop_path(
+            self.gamma.reshape(1, -1, 1, 1) * self.mlp(self.norm(self.conv_dw(x))))
+        return x
+
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+def get_dwconv(dim, kernel, bias):
+    return nn.Conv2d(dim, dim, kernel_size=kernel, padding=(kernel-1)//2 ,bias=bias, groups=dim)
+
+class gnconv(nn.Module):
+    def __init__(self, dim, order=5, gflayer=None, h=14, w=8, s=1.0):
+        super().__init__()
+        self.order = order
+        self.dims = [dim // 2 ** i for i in range(order)]
+        self.dims.reverse()
+        self.proj_in = nn.Conv2d(dim, 2*dim, 1)
+
+        if gflayer is None:
+            self.dwconv = get_dwconv(sum(self.dims), 7, True)
+        else:
+            self.dwconv = gflayer(sum(self.dims), h=h, w=w)
+        
+        self.proj_out = nn.Conv2d(dim, dim, 1)
+
+        self.pws = nn.ModuleList(
+            [nn.Conv2d(self.dims[i], self.dims[i+1], 1) for i in range(order-1)]
+        )
+
+        self.scale = s
+        print('[gnconv]', order, 'order with dims=', self.dims, 'scale=%.4f'%self.scale)
+
+    def forward(self, x, mask=None, dummy=False):
+        fused_x = self.proj_in(x)
+        pwa, abc = torch.split(fused_x, (self.dims[0], sum(self.dims)), dim=1)
+
+        dw_abc = self.dwconv(abc) * self.scale
+
+        dw_list = torch.split(dw_abc, self.dims, dim=1)
+        x = pwa * dw_list[0]
+
+        for i in range(self.order -1):
+            x = self.pws[i](x) * dw_list[i+1]
+
+        x = self.proj_out(x)
+
+        return x
+
+class HorBlock(nn.Module):
+    """ HorNet block """
+
+    def __init__(self, dim, order=4, mlp_ratio=4, drop_path=0., init_value=1e-6, gnconv=gnconv):
+        super().__init__()
+
+        self.norm1 = LayerNorm(dim, eps=1e-6, data_format='channels_first')
+        self.gnconv = gnconv(dim, order)  # depthwise conv
+        self.norm2 = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, int(mlp_ratio * dim))  # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(int(mlp_ratio * dim), dim)
+        self.gamma1 = nn.Parameter(init_value * torch.ones(dim), requires_grad=True)
+        self.gamma2 = nn.Parameter(init_value * torch.ones((dim)), requires_grad=True)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        B, C, H, W  = x.shape
+        gamma1 = self.gamma1.view(C, 1, 1)
+        x = x + self.drop_path(gamma1 * self.gnconv(self.norm1(x)))
+
+        input = x
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm2(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma2 is not None:
+            x = self.gamma2 * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+
+class HorNetSubBlock(HorBlock):
+    """A block of HorNet."""
+
+    def __init__(self, dim, mlp_ratio=4., drop_path=0.1, init_value=1e-6):
+        super().__init__(dim, mlp_ratio=mlp_ratio, drop_path=drop_path, init_value=init_value)
+        self.apply(self._init_weights)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'gamma1', 'gamma2'}
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+
+class GroupConv2d(nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=0,
+                 groups=1,
+                 act_norm=False,
+                 act_inplace=True):
+        super(GroupConv2d, self).__init__()
+        self.act_norm=act_norm
+        if in_channels % groups != 0:
+            groups=1
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size=kernel_size,
+            stride=stride, padding=padding, groups=groups)
+        self.norm = nn.GroupNorm(groups,out_channels)
+        self.activate = nn.LeakyReLU(0.2, inplace=act_inplace)
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.act_norm:
+            y = self.activate(self.norm(y))
+        return y
+
+
+class gInception_ST(nn.Module):
+    """A IncepU block for SimVP"""
+
+    def __init__(self, C_in, C_hid, C_out, incep_ker = [3,5,7,11], groups = 8):        
+        super(gInception_ST, self).__init__()
+        self.conv1 = nn.Conv2d(C_in, C_hid, kernel_size=1, stride=1, padding=0)
+
+        layers = []
+        for ker in incep_ker:
+            layers.append(GroupConv2d(
+                C_hid, C_out, kernel_size=ker, stride=1,
+                padding=ker//2, groups=groups, act_norm=True))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        y = 0
+        for layer in self.layers:
+            y += layer(x)
+        return y
+
+
+class BasicConv2d(nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 upsampling=False,
+                 act_norm=False,
+                 act_inplace=True):
+        super(BasicConv2d, self).__init__()
+        self.act_norm = act_norm
+        if upsampling is True:
+            self.conv = nn.Sequential(*[
+                nn.Conv2d(in_channels, out_channels*4, kernel_size=kernel_size,
+                          stride=1, padding=padding, dilation=dilation),
+                nn.PixelShuffle(2)
+            ])
+        else:
+            self.conv = nn.Conv2d(
+                in_channels, out_channels, kernel_size=kernel_size,
+                stride=stride, padding=padding, dilation=dilation)
+
+        self.norm = nn.GroupNorm(2, out_channels)
+        self.act = nn.SiLU(inplace=act_inplace)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d)):
+            trunc_normal_(m.weight, std=.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.act_norm:
+            y = self.act(self.norm(y))
+        return y
+
+
+class ConvSC(nn.Module):
+
+    def __init__(self,
+                 C_in,
+                 C_out,
+                 kernel_size=3,
+                 downsampling=False,
+                 upsampling=False,
+                 act_norm=True,
+                 act_inplace=True):
+        super(ConvSC, self).__init__()
+
+        stride = 2 if downsampling is True else 1
+        padding = (kernel_size - stride + 1) // 2
+
+        self.conv = BasicConv2d(C_in, C_out, kernel_size=kernel_size, stride=stride,
+                                upsampling=upsampling, padding=padding,
+                                act_norm=act_norm, act_inplace=act_inplace)
+
+    def forward(self, x):
+        y = self.conv(x)
+        return y
+
+
+class SimVP_Model(nn.Module):
+    r"""SimVP Model
+
+    Implementation of `SimVP: Simpler yet Better Video Prediction
+    <https://arxiv.org/abs/2206.05099>`_.
+
+    """
+
+    def __init__(self, in_shape, hid_S=16, hid_T=256, N_S=4, N_T=4, model_type='gSTA',
+                 mlp_ratio=8., drop=0.0, drop_path=0.0, spatio_kernel_enc=3,
+                 spatio_kernel_dec=3, act_inplace=True, **kwargs):
+        super(SimVP_Model, self).__init__()
+        T, C, H, W = in_shape  # T is pre_seq_length
+        H, W = int(H / 2**(N_S/2)), int(W / 2**(N_S/2))  # downsample 1 / 2**(N_S/2)
+        act_inplace = False
+        self.enc = Encoder(C, hid_S, N_S, spatio_kernel_enc, act_inplace=act_inplace)
+        self.dec = Decoder(hid_S, C, N_S, spatio_kernel_dec, act_inplace=act_inplace)
+
+        model_type = 'gsta' if model_type is None else model_type.lower()
+        if model_type == 'incepu':
+            self.hid = MidIncepNet(T*hid_S, hid_T, N_T)
+        else:
+            self.hid = MidMetaNet(T*hid_S, hid_T, N_T,
+                input_resolution=(H, W), model_type=model_type,
+                mlp_ratio=mlp_ratio, drop=drop, drop_path=drop_path)
+
+    def forward(self, x_raw, **kwargs):
+        B, T, C, H, W = x_raw.shape
+        x = x_raw.view(B*T, C, H, W)
+
+        embed, skip = self.enc(x)
+        _, C_, H_, W_ = embed.shape
+
+        z = embed.view(B, T, C_, H_, W_)
+        hid = self.hid(z)
+        hid = hid.reshape(B*T, C_, H_, W_)
+
+        Y = self.dec(hid, skip)
+        Y = Y.reshape(B, T, C, H, W)
+        return Y
+
+
+def sampling_generator(N, reverse=False):
+    samplings = [False, True] * (N // 2)
+    if reverse: return list(reversed(samplings[:N]))
+    else: return samplings[:N]
+
+
+class Encoder(nn.Module):
+    """3D Encoder for SimVP"""
+
+    def __init__(self, C_in, C_hid, N_S, spatio_kernel, act_inplace=True):
+        samplings = sampling_generator(N_S)
+        super(Encoder, self).__init__()
+        self.enc = nn.Sequential(
+              ConvSC(C_in, C_hid, spatio_kernel, downsampling=samplings[0],
+                     act_inplace=act_inplace),
+            *[ConvSC(C_hid, C_hid, spatio_kernel, downsampling=s,
+                     act_inplace=act_inplace) for s in samplings[1:]]
+        )
+
+    def forward(self, x):  # B*4, 3, 128, 128
+        enc1 = self.enc[0](x)
+        latent = enc1
+        for i in range(1, len(self.enc)):
+            latent = self.enc[i](latent)
+        return latent, enc1
+
+
+class Decoder(nn.Module):
+    """3D Decoder for SimVP"""
+
+    def __init__(self, C_hid, C_out, N_S, spatio_kernel, act_inplace=True):
+        samplings = sampling_generator(N_S, reverse=True)
+        super(Decoder, self).__init__()
+        self.dec = nn.Sequential(
+            *[ConvSC(C_hid, C_hid, spatio_kernel, upsampling=s,
+                     act_inplace=act_inplace) for s in samplings[:-1]],
+              ConvSC(C_hid, C_hid, spatio_kernel, upsampling=samplings[-1],
+                     act_inplace=act_inplace)
+        )
+        self.readout = nn.Conv2d(C_hid, C_out, 1)
+
+    def forward(self, hid, enc1=None):
+        for i in range(0, len(self.dec)-1):
+            hid = self.dec[i](hid)
+        Y = self.dec[-1](hid + enc1)
+        Y = self.readout(Y)
+        return Y
+
+
+class MidIncepNet(nn.Module):
+    """The hidden Translator of IncepNet for SimVPv1"""
+
+    def __init__(self, channel_in, channel_hid, N2, incep_ker=[3,5,7,11], groups=8, **kwargs):
+        super(MidIncepNet, self).__init__()
+        assert N2 >= 2 and len(incep_ker) > 1
+        self.N2 = N2
+        enc_layers = [gInception_ST(
+            channel_in, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups)]
+        for i in range(1,N2-1):
+            enc_layers.append(
+                gInception_ST(channel_hid, channel_hid//2, channel_hid,
+                              incep_ker=incep_ker, groups=groups))
+        enc_layers.append(
+                gInception_ST(channel_hid, channel_hid//2, channel_hid,
+                              incep_ker=incep_ker, groups=groups))
+        dec_layers = [
+                gInception_ST(channel_hid, channel_hid//2, channel_hid,
+                              incep_ker=incep_ker, groups=groups)]
+        for i in range(1,N2-1):
+            dec_layers.append(
+                gInception_ST(2*channel_hid, channel_hid//2, channel_hid,
+                              incep_ker=incep_ker, groups=groups))
+        dec_layers.append(
+                gInception_ST(2*channel_hid, channel_hid//2, channel_in,
+                              incep_ker=incep_ker, groups=groups))
+
+        self.enc = nn.Sequential(*enc_layers)
+        self.dec = nn.Sequential(*dec_layers)
+
+    def forward(self, x):
+        B, T, C, H, W = x.shape
+        x = x.reshape(B, T*C, H, W)
+
+        # encoder
+        skips = []
+        z = x
+        for i in range(self.N2):
+            z = self.enc[i](z)
+            if i < self.N2-1:
+                skips.append(z)
+        # decoder
+        z = self.dec[0](z)
+        for i in range(1,self.N2):
+            z = self.dec[i](torch.cat([z, skips[-i]], dim=1) )
+
+        y = z.reshape(B, T, C, H, W)
+        return y
+
+
+class MetaBlock(nn.Module):
+    """The hidden Translator of MetaFormer for SimVP"""
+
+    def __init__(self, in_channels, out_channels, input_resolution=None, model_type=None,
+                 mlp_ratio=8., drop=0.0, drop_path=0.0, layer_i=0):
+        super(MetaBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        model_type = model_type.lower() if model_type is not None else 'gsta'
+
+        if model_type == 'convnext':
+            self.block = ConvNeXtSubBlock(
+                in_channels, mlp_ratio=mlp_ratio, drop=drop, drop_path=drop_path)
+        elif model_type == 'hornet':
+            self.block = HorNetSubBlock(in_channels, mlp_ratio=mlp_ratio, drop_path=drop_path)
+        else:
+            assert False and "Invalid model_type in SimVP"
+
+        if in_channels != out_channels:
+            self.reduction = nn.Conv2d(
+                in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        z = self.block(x)
+        return z if self.in_channels == self.out_channels else self.reduction(z)
+
+
+class MidMetaNet(nn.Module):
+    """The hidden Translator of MetaFormer for SimVP"""
+
+    def __init__(self, channel_in, channel_hid, N2,
+                 input_resolution=None, model_type=None,
+                 mlp_ratio=4., drop=0.0, drop_path=0.1):
+        super(MidMetaNet, self).__init__()
+        assert N2 >= 2 and mlp_ratio > 1
+        self.N2 = N2
+        dpr = [  # stochastic depth decay rule
+            x.item() for x in torch.linspace(1e-2, drop_path, self.N2)]
+
+        # downsample
+        enc_layers = [MetaBlock(
+            channel_in, channel_hid, input_resolution, model_type,
+            mlp_ratio, drop, drop_path=dpr[0], layer_i=0)]
+        # middle layers
+        for i in range(1, N2-1):
+            enc_layers.append(MetaBlock(
+                channel_hid, channel_hid, input_resolution, model_type,
+                mlp_ratio, drop, drop_path=dpr[i], layer_i=i))
+        # upsample
+        enc_layers.append(MetaBlock(
+            channel_hid, channel_in, input_resolution, model_type,
+            mlp_ratio, drop, drop_path=drop_path, layer_i=N2-1))
+        self.enc = nn.Sequential(*enc_layers)
+
+    def forward(self, x):
+        B, T, C, H, W = x.shape
+        x = x.reshape(B, T*C, H, W)
+
+        z = x
+        for i in range(self.N2):
+            z = self.enc[i](z)
+
+        y = z.reshape(B, T, C, H, W)
+        return y
